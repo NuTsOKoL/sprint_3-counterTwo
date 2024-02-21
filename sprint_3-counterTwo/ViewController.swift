00ea5db5e4a11c008c1a7dc95f6b4d7c3d514c6e
NuTsOKoL <mi.sei@yandex.ru>
2024-02-21 04:49:47 +0300

    adding solution

--- sprint_3-counterTwo/ViewController.swift
+++ sprint_3-counterTwo/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var dataLabel: UILabel!
+    @IBOutlet weak var storyButtons: UITextView!
+    
+    var count = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        minusButton.tintColor = .blue
+        plusButton.tintColor = .red
+        resetButton.setImage(UIImage(systemName: "restart"), for: .normal)
+        storyButtons.isEditable = false
     }
-
-
+    
+    private func currentDate() -> String {
+        let dateFormat = DateFormatter()
+        dateFormat.dateFormat = "dd-mm-yyyy HH:mm:ss"
+        let date = dateFormat.string(from: Date())
+        return date
+    }
+    
+    @IBAction private func minusTouchButton(_ sender: Any) {
+        //        count.minValue = 0
+        count = count - 1
+        dataLabel.text = "\(count)"
+        storyButtons.text += "\n\(currentDate()): значение изменено на \(count)"
+        
+    }
+    @IBAction private func plusTouchButton(_ sender: Any) {
+        count = count + 1
+        dataLabel.text = "\(count)"
+        storyButtons.text += "\n\(currentDate()): значение изменено на \(count)"
+    }
+    @IBAction private func resetTouchButton(_ sender: Any) {
+        count = 0
+        dataLabel.text = "\(count)"
+        storyButtons.text += "\n\(currentDate()): значение сброшено"
+    }
 }
 
